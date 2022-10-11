dd330c7aabe15ea36d673d6b81b35d19dd7ceabc
Frank Hereford <frank@frankhereford.com>
2022-10-11 23:24:55 +0000

    piece server side functions

--- src/server/trpc/router/piece.ts
+++ src/server/trpc/router/piece.ts
@@ -1,7 +1,14 @@
 import { t, authedProcedure } from "../trpc";
 import { z } from "zod";
+import nj from "numjs"
 
 export const pieceRouter = t.router({
+  list: authedProcedure
+    .query(async ({ ctx, input }) => {
+      const pieces = await ctx.prisma.piece.findMany({ });
+      return pieces;
+    }),
+
   get: authedProcedure
     .input(z.object({ id: z.string().nullish() }).nullish())
     .query(async ({ ctx, input }) => {
@@ -9,24 +16,23 @@ export const pieceRouter = t.router({
         where: {
           id: input.id,
         }
-      })
-      return {
-        piece: piece
-      }
+      });
+      return piece;
     }),
   set: authedProcedure
     .input(z.object({ color: z.string().nullish() }).nullish())
-    .query(async ({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       //console.log(ctx)
       //console.log(input)
+      shape = nj.zeros([5,5])
       const piece = await ctx.prisma.piece.create({
         data: {
           color: input.color,
-          shape: ['one', 'two'],
-          userId: ctx.session.user.id
+          shape: shape,
+          userId: ctx.session.user.id,
         },
-      })
-      return { piece: piece}
+      });
+      return piece;
     }),
   getAll: t.procedure.query(({ ctx }) => {
     return ctx.prisma.piece.findMany();
