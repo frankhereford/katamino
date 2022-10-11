d5ac1f4385682cf77faecc082f862bfd8974adb6
Frank Hereford <frank@frankhereford.com>
2022-10-11 23:24:46 +0000

    adding back example

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -2,10 +2,12 @@
 import { t } from "../trpc";
 import { pieceRouter } from "./piece";
 import { authRouter } from "./auth";
+import { exampleRouter } from "./example";
 
 export const appRouter = t.router({
   piece: pieceRouter,
   auth: authRouter,
+  example: exampleRouter,
 });
 
 // export type definition of API
