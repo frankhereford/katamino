4846445e20f68cda0b9a3f45f23228c78f534279
Frank Hereford <frank@frankhereford.com>
2022-10-13 00:51:42 +0000

    gridboard drawing shapes

--- src/pages/components/GridBoard.tsx
+++ src/pages/components/GridBoard.tsx
@@ -3,22 +3,20 @@ import GridSquare from './GridSquare'
 
 // Represents a 10 x 18 grid of grid squares
 
-export default function GridBoard(props) {
+export default function GridBoard(props: { piece: any; color: any; size: string; }) {
 
-    // generates an array of 18 rows, each containing 10 GridSquares.
-
-    const cols = 5;
+    const cols = props.piece[0].length;
 
     const grid = []
-    for (let row = 0; row < 5; row++) {
+    for (let row = 0; row < props.piece.length; row++) {
         grid.push([])
-        for (let col = 0; col < cols; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color={`${props.color}`} />)
+        for (let col = 0; col < props.piece[row].length; col++) {
+            grid[row].push(<GridSquare key={`${col}${row}`} color={`${props.piece[row][col] ? 'red' : props.color}`} />)
         }
     }
 
     return (
-        <div className={"grid-board " + props.size}>
+        <div className={"grid-board " + props.board_type} style={{ '--cols': props.piece[0].length }}>
             {grid}
         </div>
     )
