2fe8bb2d2d01a718d2a2171f7a2f1ef5f1ecb85a
Frank Hereford <frank@frankhereford.com>
2022-10-13 01:49:59 +0000

    a onclick handler

--- src/pages/components/GridSquare.tsx
+++ src/pages/components/GridSquare.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 // Represents a grid square with a color
 
 export default function GridSquare(props) {
-    const classes = `grid-square color-${props.color}`
-    return <div className={classes} />
+
+  const clickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(props.row);
+    console.log(props.col);
+  }
+
+  const classes = `grid-square color-${props.color}`
+  return <div onClick={clickHandler} className={classes} />
 }
