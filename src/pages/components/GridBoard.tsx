d4fe045572885004ae2a57345586978676128765
Frank Hereford <frank@frankhereford.com>
2022-10-13 01:10:20 +0000

    linting insanity

--- src/pages/components/GridBoard.tsx
+++ src/pages/components/GridBoard.tsx
@@ -3,19 +3,31 @@ import GridSquare from './GridSquare'
 
 // Represents a 10 x 18 grid of grid squares
 
-export default function GridBoard(props: { piece: any; color: any; size: string; }) {
+export default function GridBoard(props : {
+    piece : any;
+    color : any;
+    size : string;
+}) {
 
-    const grid = []
-    for (let row = 0; row < props.piece.length; row++) {
-        grid.push([])
-        for (let col = 0; col < props.piece[row].length; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color={`${props.piece[row][col] ? 'red' : props.color}`} />)
-        }
+  const grid = []
+  for (let row = 0; row < props.piece.length; row++) {
+    grid.push([])
+    for (let col = 0; col < props.piece[row].length; col++) {
+      grid[row].push (
+      <GridSquare key={`${col}${row}` } color={ `${ props.piece[row][col] ? 'red' : props.color }` }/>
+      )
     }
+  }
 
-    return (
-        <div className={"grid-board " + props.board_type} style={{ '--cols': props.piece[0].length }}>
-            {grid}
-        </div>
-    )
+  return (
+    <div className="grid-board"
+      style={
+        {
+          '--cols': props.piece[0].length,
+          '--tile-size': '40px',
+          '--border-width': '2px'
+        }
+      }>
+    {grid} </div>
+  )
 }
