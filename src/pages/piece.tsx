843e279026b6d84be116d2c29ac60c1473569e44
Frank Hereford <frank@frankhereford.com>
2022-10-13 00:51:59 +0000

    improve some types and a test shape

--- src/pages/piece.tsx
+++ src/pages/piece.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
-import GridSquare from './components/GridSquare'
 import GridBoard from './components/GridBoard'
 
 const Piece: NextPage = () => {
   const pieces = trpc.piece.list.useQuery();  
+
+  const shape = [
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [0,1,1,1,0],
+    [0,0,1,0,0],
+    [0,0,0,0,0],
+  ]
 
   return (
     <>
@@ -30,7 +36,7 @@ const Piece: NextPage = () => {
               <tr key={index}>
                 <td>{piece.id}</td>
                 <td>{piece.color}</td>
-                <td>{piece.shape}</td>
+                <td>{JSON.stringify(piece.shape)}</td>
               </tr>
             ))
             }
@@ -38,7 +44,7 @@ const Piece: NextPage = () => {
         </table>
       </div>
     
-      <GridBoard color='grey' size='piece'/>
+      <GridBoard color='grey' board_type='piece' piece={shape} />
 
     </>
   );
