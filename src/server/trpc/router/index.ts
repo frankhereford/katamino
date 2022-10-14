74d6c722bc62706280444b9196e72191d9188e05
Frank Hereford <frank@frankhereford.com>
2022-10-14 02:14:38 +0000

    use color router

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,11 +1,13 @@
 // src/server/trpc/router/index.ts
 import { t } from "../trpc";
 import { pieceRouter } from "./piece";
+import { colorRouter } from "./color";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
 
 export const appRouter = t.router({
   piece: pieceRouter,
+  color: colorRouter,
   auth: authRouter,
   example: exampleRouter,
 });
