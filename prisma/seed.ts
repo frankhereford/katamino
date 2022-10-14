1c4ef1f55f9d96040880f84fb1ca618a602f0a8d
Frank Hereford <frank@frankhereford.com>
2022-10-14 02:11:16 +0000

    fix seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-  const alice = await prisma.color.upsert({
-    where: { name: 'red' },
-    update: {},
-    create: {
-      name: 'red',
+  const colors = ['red', 'green', 'dark-grey', 'purple', 'brown', 'blue', 'dark-green', 'yellow', 'teal']
+  colors.forEach(async (color) => {
+    await prisma.color.upsert({
+      where: { name: color },
+      update: {},
+      create: {
+        name: color,
       },
     },
-  )
+    )
+  })
+
 }
 main()
   .then(async () => {
