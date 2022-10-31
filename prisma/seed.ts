c38702f303ed232a7f7dffb7681ea83802fcd74a
Frank Hereford <frank@frankhereford.com>
2022-10-31 23:22:45 +0000

    seed the pieces

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,12 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
 
+  const user = await prisma.user.findFirst({
+    where: {
+      name: 'Frank Hereford',
+    }
+  })
+ 
   const colors = [
     'red', 'green', 'dark-grey',
     'purple', 'brown', 'blue',
@@ -19,6 +25,102 @@ async function main() {
     },
     )
   })
+
+  const deletePieces = await prisma.piece.deleteMany({})
+
+  const pieces = [
+    {name: 'dark-blue',
+     shape: [[0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0]]},
+    {name: 'orange',
+     shape: [[0,0,0,0,0],
+             [0,0,1,1,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0]]},
+    {name: 'brown',
+     shape: [[0,0,0,0,0],
+             [0,1,0,0,0],
+             [0,1,1,0,0],
+             [0,1,0,0,0],
+             [0,1,0,0,0]]},
+    {name: 'purple',
+     shape: [[0,0,0,0,0],
+             [0,0,0,1,0],
+             [0,0,0,1,0],
+             [0,0,1,1,0],
+             [0,0,1,0,0]]},
+    {name: 'blue',
+     shape: [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,0,0,0],
+             [0,1,0,0,0],
+             [0,0,0,0,0]]},
+    {name: 'pink',
+     shape: [[0,0,0,0,0],
+             [0,1,0,0,0],
+             [0,1,1,0,0],
+             [0,1,1,0,0],
+             [0,0,0,0,0]]},
+    {name: 'yellow',
+     shape: [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,0,1,0],
+             [0,0,0,0,0],
+             [0,0,0,0,0]]},
+    {name: 'teal',
+     shape: [[0,0,0,0,0],
+             [0,0,0,1,0],
+             [0,1,1,1,0],
+             [0,1,0,0,0],
+             [0,0,0,0,0]]},
+    {name: 'dark-grey',
+     shape: [[0,0,0,0,0],
+             [0,0,1,0,0],
+             [0,1,1,1,0],
+             [0,1,0,0,0],
+             [0,0,0,0,0]]},
+    {name: 'dark-green',
+     shape: [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,0,0,0]]},
+    {name: 'green',
+     shape: [[0,0,0,0,0],
+             [0,0,0,1,0],
+             [0,0,1,1,0],
+             [0,1,1,0,0],
+             [0,0,0,0,0]]},
+    {name: 'red',
+     shape: [[0,0,0,0,0],
+             [0,0,1,0,0],
+             [0,1,1,1,0],
+             [0,0,1,0,0],
+             [0,0,0,0,0]]},
+  ]
+
+  pieces.forEach(async (piece) => {
+    const color = await prisma.color.findFirst({
+      where: {
+        name: piece.name,
+      }
+    })
+ 
+    await prisma.piece.create({
+      data: {
+        shape: piece.shape,
+        color: { connect: { id: color?.id }, },
+        user: { connect: { id: user?.id }, }
+      }
+    })
+  })
+
+
+
 
 }
 
