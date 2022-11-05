1a506b1ed840a2a3a7e890d65d1b9b07d678a3f0
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-05 07:35:06 -0500

    not gonna need this anymore

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,22 +2,7 @@ import {colord, extend} from "colord";
 import mixPlugin from "colord/plugins/mix";
 extend([mixPlugin]);
 
-export const colors = {
-  white: "#ffffff",
-  grey: "#808080",
-  red: "#b40d0d",
-  green: "#95ff85",
-  darkGrey: "#353535",
-  purple: "#66009d",
-  brown: "#583200",
-  darkBlue: "#0222b1",
-  darkGreen: "#004d00",
-  yellow: "#f9d237",
-  teal: "#00b3b3",
-  blue: "#39c9f1",
-  orange: "#ff6600",
-  pink: "#ff47c8"
-}
+
 
 // https://gist.github.com/cawfree/c08c10f6f2e7b2c8d225d88b031a03ce
 export const toCamelCase = (e: string) => {
