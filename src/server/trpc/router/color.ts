f102a241d4f9fa3fdb13ea5d13619effd5855e1b
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-05 08:54:37 -0500

    one more down

--- src/server/trpc/router/color.ts
+++ src/server/trpc/router/color.ts
@@ -1,13 +1,12 @@
-import { z } from "zod";
-
+import { Color } from '@prisma/client'
 import { router, publicProcedure } from "../trpc";
 
 export const colorRouter = router({
 
   randomColor: publicProcedure
     //.input(z.object({ text: z.string().nullish() }).nullish())
-    .query(async ({ }) => {
-      const randomColors = await prisma.$queryRaw`SELECT * FROM colors order by random() limit 1;`;
+    .query(async ({ ctx }) => {
+      const randomColors = await ctx.prisma.$queryRaw<Color[]>`SELECT * FROM colors order by random() limit 1;`;
       return randomColors[0]
     }),
 
