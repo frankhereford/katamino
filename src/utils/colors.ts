46bc42ecd4ea6d82b977e6a5ae1dfe51f7a0d7e0
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-04 23:30:22 -0500

    tighter typing

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -25,9 +25,9 @@ export const toCamelCase = (e: string) => {
 }
 
 export const toSnakeCase = (e: any) => {
-  return e.match(/([A-Z])/g).reduce((str: any, c: any) => str.replace(new RegExp(c), '_' + c.toLowerCase()), e).substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0)
+  return e.match(/([A-Z])/g).reduce((str: string, c: string) => str.replace(new RegExp(c), '_' + c.toLowerCase()), e).substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0)
 }
 
-export function mix_colors(color1: any, color2: any) {
+export function mix_colors(color1: string, color2: string) {
   return colord(color1).mix(colord(color2), .5).darken(.05).toHex()
 }
