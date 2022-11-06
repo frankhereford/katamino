1e154838b6331d7e5a3efc8be8f80995f9be9f26
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-06 07:11:35 -0600

    going to redo this, want to get going

--- src/pages/components/Piece.tsx
+++ src/pages/components/Piece.tsx
@@ -7,21 +7,22 @@ interface PieceProps {
   id?: string;
 }
 
-export default function Piece(props: PieceProps) {
+export default function PiecePage(props: PieceProps) {
 
-  const [piece, setPiece] = useState()
+  const [piece, setPiece] = useState() // somehow tell it that it's a piece
   const { data: randomPiece } = trpc.piece.randomPiece.useQuery(undefined, {enabled: (!piece && !props.id)});
   const { data: propsPiece } = trpc.piece.getPiece.useQuery({ id: props.id }, {enabled: (props.id != null)});
 
   useEffect(() => {
-    if (!piece && props.id) {
-      console.log('just need to get a particular piece')
-        console.log(propsPiece)
-        setPiece(propsPiece)
+    if (!piece?.id && props.id) {
+      //console.log('just need to get a particular piece')
+      //console.log(propsPiece)
+      //console.log(typeof propsPiece)
+      setPiece(propsPiece)
     }
-    if (!piece && !props.id) {
-      console.log('just need to get a random piece')
-      console.log(randomPiece)
+    else if (!piece?.id && !props.id) {
+      //console.log('just need to get a random piece')
+      //console.log(randomPiece)
       setPiece(randomPiece)
     }
   })
