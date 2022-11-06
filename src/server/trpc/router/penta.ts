33dd61b25d1a2d843b947eace62ce41b7fc8e49f
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-06 14:57:37 -0600

    add get penta method

--- src/server/trpc/router/penta.ts
+++ src/server/trpc/router/penta.ts
@@ -3,6 +3,31 @@ import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const pentaRouter = router({
 
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+    return await ctx.prisma.penta.findFirst({
+      where: {
+        user: ctx.session.user,
+        id: input.id
+      },
+      include: {
+        blocks: {
+          include: {
+            piece: {
+              include: {
+                color: true
+              }
+            }
+          },
+        }
+      },
+      orderBy: {
+        id: "asc"
+      }
+    });
+  }),
+
   getAll: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.penta.findMany({
       where: {
