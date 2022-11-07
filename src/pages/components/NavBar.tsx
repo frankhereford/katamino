611db1e2e771a5748cae07db32ef11b949c51cb9
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-07 13:59:51 -0600

    with userdata and rounded corners

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function NavBar() {
-
+  const { data: sessionData } = useSession();
+  console.log(sessionData)
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-primary rounded-b-md bg-base-100 mb-2">
         <div className="flex-1">
           <Link href="/" className="btn btn-ghost normal-case text-xl">Katamino</Link>
         </div>
@@ -18,7 +20,7 @@ export default function NavBar() {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
+                <img src={sessionData?.user?.image || ""} />
               </div>
             </label>
             <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
