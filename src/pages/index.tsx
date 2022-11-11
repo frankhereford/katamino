744cf0888bf92e4e41b7bb8437749faf73ef13bc
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-11 01:00:59 -0600

    11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           Play <span className="text-primary">K</span>atamino
         </h1>
         <div>
-          Test message 10 
+          Test message 11 
         </div>
 
         <h3 className="mt-2">A square</h3>
