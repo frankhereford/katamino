c2e800b747b4fca6cdd422ec6ed4db9b1892a0e5
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-10 23:10:37 -0600

    nine in the mine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           Play <span className="text-primary">K</span>atamino
         </h1>
         <div>
-          Test message 8
+          Test message 9
         </div>
 
         <h3 className="mt-2">A square</h3>
