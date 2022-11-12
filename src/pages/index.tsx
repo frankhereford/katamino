1a0beab6e45f7b60577680eff8a4cc7815bd5ceb
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-12 09:42:41 -0600

    Back to the CI system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           Play <span className="text-primary">K</span>atamino
         </h1>
         <div>
-          Test message 11 
+          Test message 12 
         </div>
 
         <h3 className="mt-2">A square</h3>
