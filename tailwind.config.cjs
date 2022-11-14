36584661480d4fe7b186efb873aa78bf52386910
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-13 23:15:13 -0600

    fix rending of more wide column widths

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateColumns: {
-        // Simple 16 column grid
+        // adding more of these for my widest grids
+        13: "repeat(13, minmax(0, 1fr))",
+        14: "repeat(14, minmax(0, 1fr))",
+        14: "repeat(15, minmax(0, 1fr))",
         16: "repeat(16, minmax(0, 1fr))",
       },
     },
