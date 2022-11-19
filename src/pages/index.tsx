56262c8b151c6ce2aea39247750514c189b48b43
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-18 18:54:36 -0600

    👋 linter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,7 @@ const Home: NextPage = (props: any) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
   const auth0 = props.providers?.auth0!
 
+
   return (
     <>
       <HeaderContent />
