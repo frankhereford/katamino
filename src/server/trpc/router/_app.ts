a576529bc9756f623210b5a87bf7d7be38983691
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-20 14:26:36 -0600

    add piece router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,8 +2,10 @@ import { router } from '../trpc'
 import { authRouter } from './auth'
 // import { exampleRouter } from './example'
 import { colorRouter } from './color'
+import { pieceRouter } from './piece'
 
 export const appRouter = router({
+  piece: pieceRouter,
   color: colorRouter,
   auth: authRouter
 })
