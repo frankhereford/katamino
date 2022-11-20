441e67b59609fd619c4680a7b66c091ead0ebca8
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-20 10:48:02 -0600

    front page

--- src/pages/components/HeaderContent.tsx
+++ src/pages/components/HeaderContent.tsx
@@ -1,12 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head'
 
-export default function HeaderContent(props: { description?: string }) {
-
+export default function HeaderContent (props: { description?: string }): JSX.Element {
   return (
     <Head>
       <title>Katamino</title>
-      <meta name="description" content="A T3 Stack Experiment" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name='description' content={props.description ?? 'A T3 Stack Experiment'} />
+      <link rel='icon' href='/favicon.ico' />
     </Head>
-  );
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,38 @@
 import { type NextPage } from 'next'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 
-import { AiOutlineArrowDown } from 'react-icons/ai';
+import { AiOutlineArrowDown } from 'react-icons/ai'
 
-import HeaderContent from "./components/HeaderContent";
-
-import { trpc } from '../utils/trpc'
+import HeaderContent from './components/HeaderContent'
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: 'from tRPC' })
-
   return (
     <>
       <HeaderContent />
-      <main className="container mx-auto flex flex-col items-center p-4">
+      <main className='container mx-auto flex flex-col items-center p-4'>
 
-        <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem] ">
-          <span className="text-primary">K</span>atamino
+        <h1 className='text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem] '>
+          <span className='text-primary'>K</span>atamino
         </h1>
 
-        <div className="relative left-[75px] bottom-[-10px] animate-bounce mt-[40px]">
-          <AiOutlineArrowDown size={170} style={{ color: "#dddddd" }} />
+        <div className='relative left-[75px] bottom-[-10px] animate-bounce mt-[40px]'>
+          <AiOutlineArrowDown size={170} style={{ color: '#dddddd' }} />
         </div>
 
-
-        <div className="grid grid-cols-2 gap-10 mb-2">
-          <button onClick={() => signIn(auth0.id)} className="btn btn-primary btn-lg drop-shadow-lg">
+        <div className='grid grid-cols-2 gap-10 mb-2'>
+          <button onClick={() => { void signIn('auth0') }} className='btn btn-primary btn-lg drop-shadow-lg'>
             Auth0
           </button>
-          <button onClick={() => signIn(github.id)} className="btn btn-primary btn-lg drop-shadow-lg">
+          <button onClick={() => { void signIn('github') }} className='btn btn-primary btn-lg drop-shadow-lg'>
             GitHub
           </button>
         </div>
 
-        <h1 className="ml-[175px] text-[5rem] leading-normal text-gray-400">
-          <span className="text-primary font-extrabold drop-shadow-lg">L</span>ogin
+        <h1 className='ml-[175px] text-[5rem] leading-normal text-gray-400'>
+          <span className='text-primary font-extrabold drop-shadow-lg'>L</span>ogin
         </h1>
 
-        <div className="relative top-[-140px] left-[30px] text-primary text-[250px] rotate-90 drop-shadow-lg">
+        <div className='relative top-[-140px] left-[30px] text-primary text-[250px] rotate-90 drop-shadow-lg'>
           )
         </div>
 
@@ -47,27 +42,3 @@ const Home: NextPage = () => {
 }
 
 export default Home
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession()
-
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  )
-
-  return (
-    <div className='flex flex-col items-center justify-center gap-4'>
-      <p className='text-center text-2xl text-white'>
-        {(sessionData != null) && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className='rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
-        onClick={(sessionData != null) ? async () => await signOut() : async () => await signIn()}
-      >
-        {(sessionData != null) ? 'Sign out' : 'Sign in'}
-      </button>
-    </div>
-  )
-}
