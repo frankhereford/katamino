423ec68b9d08b7749e08eeba02e07a54a9c4e133
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-22 14:14:37 -0600

    add no translation option to transformation

--- src/utils/transformations.ts
+++ src/utils/transformations.ts
@@ -2,15 +2,19 @@ import { type Prisma } from '@prisma/client'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Array2D = require('array2d')
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function transformBlockShape (shape: number[][], transformation: Prisma.TransformationGetPayload<{}>, borderWidth: number) {
+export function transformBlockShape (
+  shape: number[][],
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  transformation: Prisma.TransformationGetPayload<{}>,
+  borderWidth: number,
+  doTranslation: boolean) {
   if (transformation.reflection) {
     shape = Array2D.flip(shape, Array2D.AXES.X)
   }
   for (let i = 0; i < transformation.rotation; i++) {
     shape = Array2D.rotate(shape, Array2D.DIRECTIONS.RIGHT)
   }
-  if (transformation.translationUp != null || transformation.translationRight != null) {
+  if (doTranslation && (transformation.translationUp != null || transformation.translationRight != null)) {
     for (let i = 0; i < borderWidth; i++) {
       shape = Array2D.pad(shape, Array2D.EDGES.TOP, 1, 0)
       shape = Array2D.pad(shape, Array2D.EDGES.LEFT, 1, 0)
