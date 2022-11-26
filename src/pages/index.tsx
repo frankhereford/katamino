55bb364e2cbda233318f63241a6f03fde717f14c
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-26 13:33:13 -0600

    strip out old content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-multiple-empty-lines */
 import { useEffect } from 'react'
 import { type NextPage } from 'next'
 import { signIn, useSession } from 'next-auth/react'
@@ -20,30 +21,7 @@ const Home: NextPage = () => {
       <HeaderContent />
       <main className='container mx-auto flex flex-col items-center p-4'>
 
-        <h1 className='text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem] '>
-          <span className='text-primary'>K</span>atamino
-        </h1>
 
-        <div className='relative left-[75px] bottom-[-10px] animate-bounce mt-[40px]'>
-          <AiOutlineArrowDown size={170} style={{ color: '#dddddd' }} />
-        </div>
-
-        <div className='grid grid-cols-2 gap-10 mb-2'>
-          <button onClick={() => { void signIn('auth0') }} className='btn btn-primary btn-lg drop-shadow-lg'>
-            Auth0
-          </button>
-          <button onClick={() => { void signIn('github') }} className='btn btn-primary btn-lg drop-shadow-lg'>
-            GitHub
-          </button>
-        </div>
-
-        <h1 className='ml-[175px] text-[5rem] leading-normal text-gray-400'>
-          <span className='text-primary font-extrabold drop-shadow-lg'>L</span>ogin
-        </h1>
-
-        <div className='relative top-[-140px] left-[30px] text-primary text-[250px] rotate-90 drop-shadow-lg'>
-          )
-        </div>
 
       </main>
     </>
