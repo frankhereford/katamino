71a782f2e26f69a219695b504707095766c8454d
Frank Hereford <github@frankhereford.com>
2026-04-18 06:41:47 +0000

    Add HAProxy and Certbot services to Docker Compose configuration, including entrypoint script and HAProxy configuration for SSL termination. Update .gitignore to include certs directory.

---
+++ haproxy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+CERT_DIR="/etc/letsencrypt/live/katamino.frankhereford.com"
+HAPROXY_PEM="/certs/haproxy.pem"
+
+cat "$CERT_DIR/fullchain.pem" "$CERT_DIR/privkey.pem" > "$HAPROXY_PEM"
+chmod 600 "$HAPROXY_PEM"
+
+exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg -db
